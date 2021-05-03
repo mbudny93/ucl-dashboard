b0a971f04b96c20983cc014b941a1c2ac0e2e557
budnma <marcinbudny93@gmail.com>
2021-05-03 18:19:35 +0200

    add react

---
+++ src/frontend/src/pages/TeamPage.js
@@ -0,0 +1,18 @@
+import  {React, useEffect} from 'react';
+import  {MatchDetailCard} from '../components/MatchDetailCard';
+import  {MatchSmallCard} from '../components/MatchSmallCard';
+
+export const TeamPage = () => {
+
+    useEffect.
+  return (
+    <div className="TeamPage">
+        <h1>UCL dashboard</h1>
+        <MatchDetailCard/>
+        <MatchSmallCard/>
+        <MatchSmallCard/>
+        <MatchSmallCard/>
+    </div>
+  );
+}
+
